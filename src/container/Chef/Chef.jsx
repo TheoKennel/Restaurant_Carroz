4ba169ddf7731cac7f7e953dede97becd55b49a1
TheoKennel <theokennel@gmail.com>
2023-03-07 12:26:55 +0100

    Début structure html chef, rajout photo + titre + subheading

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src= {images.chef} alt ='chef' />
+    </div>
+
+    <div className='app__wrapper_infor'>
+      <SubHeading title ="Chef's Word" />
+        <h1 className='headtext__cormorant'>What we believe in</h1>
+    </div>
   </div>
 );
 
