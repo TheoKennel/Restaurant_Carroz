4eb3979a1978a8e9adbf7b761c130ab975a8934d
TheoKennel <theokennel@gmail.com>
2023-03-09 22:27:12 +0100

    Ajout galleryImages + const scroll pour gauche / droite + fin structure html section galery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -4,7 +4,21 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
+
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
 const Gallery = () => {
+    const scrollRef = React.useRef(null);
+
+    const scroll = (direction) => {
+        const { current } = scrollRef;
+
+        if (direction === 'left') {
+            current.scrollLeft -= 300;
+        } else {
+            current.scrollLeft += 300;
+        }
+    }
+
     return (
         <div className="app__gallery flex__center">
             <div className="app__gallery-content">
@@ -14,7 +28,18 @@ const Gallery = () => {
                 <button type="button" className="custom__button">View More</button>
             </div>
             <div className="app__gallery-images">
-                <div className="app__gallery-images_container" ref={scrollref}></div>
+                <div className="app__gallery-images_container" ref={scrollRef}>
+                    {galleryImages.map((image, index) => (
+                        <div className="app__gallery-images_card flex__center" key={`gallery_images-${index + 1}`}>
+                            <img src={image} alt="gallery" />
+                            <BsInstagram className="gallery__image-icon" />
+                        </div>
+                    ))}
+            <div className="app__gallery-images_arrows">
+                <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+                <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+            </div>
+                </div>
             </div>
         </div>
     );
