8de8172f5b09a01c8cef7fdca84b6f81070d4d08
TheoKennel <theokennel@gmail.com>
2023-03-09 22:47:34 +0100

    début CSS, positionnement images + content : mise en place scrollbar + caché

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -7,7 +7,7 @@ import './Gallery.css';
 
 const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
 const Gallery = () => {
-    const scrollRef = React.useRef(null);
+    const scrollRef = React.useRef(null);  // utiliser le hook useRef pour obtenir une référence à l'élément DOM de la galerie
 
     const scroll = (direction) => {
         const { current } = scrollRef;
@@ -29,16 +29,17 @@ const Gallery = () => {
             </div>
             <div className="app__gallery-images">
                 <div className="app__gallery-images_container" ref={scrollRef}>
+                    {/* boucler à travers le tableau d'images de la galerie */}
                     {galleryImages.map((image, index) => (
-                        <div className="app__gallery-images_card flex__center" key={`gallery_images-${index + 1}`}>
+                        <div className="app__gallery-images_card flex__center" key={`gallery_images-${index + 1}`}> {/* créer une div pour chaque carte d'image de la galerie */}
                             <img src={image} alt="gallery" />
                             <BsInstagram className="gallery__image-icon" />
                         </div>
                     ))}
-            <div className="app__gallery-images_arrows">
-                <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
-                <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
-            </div>
+                        <div className="app__gallery-images_arrows">
+                            <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} /> {/* afficher l'icône de flèche gauche avec un événement de clic pour faire défiler la galerie vers la gauche */}
+                            <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+                        </div>
                 </div>
             </div>
         </div>
